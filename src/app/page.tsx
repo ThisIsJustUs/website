57925490fd374bbd15f108e2deeedf7074f9f1c1
Justus Hebenstreit <justus@hebenstreit.io>
2025-05-10 18:30:47 +0100

    Added ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use server";
 
+import { Button } from "@/components/ui/button";
 import { HydrateClient, api } from "@/trpc/server";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center">
         <h1 className="font-bold text-4xl">Justus Hebenstreit</h1>
+        <Button>Click me</Button>
       </main>
     </HydrateClient>
   );
