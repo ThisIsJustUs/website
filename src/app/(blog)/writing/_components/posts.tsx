737062ebaa8dd6f826f2b0d59070e9b0c0f18ece
Justus Hebenstreit <justus@hebenstreit.io>
2025-05-11 19:04:58 +0100

    Split view

--- src/app/(blog)/writing/_components/posts.tsx
+++ src/app/(blog)/writing/_components/posts.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { getPosts } from "../_sanity/queries";
 import type { Post } from "../_sanity/types";
 import { WritingListItem } from "./writing-list-item";
@@ -12,19 +13,21 @@ export async function Posts() {
   }
 
   return (
-    <div className="mt-6 w-full">
-      {posts.map((post: Post, index: number) => (
-        <div
-          key={post.slug?.toString()}
-          className={`py-4 ${index !== posts.length - 1 ? "border-b border-gray-200" : ""}`}
-        >
-          <WritingListItem
-            title={post.title ?? ""}
-            date={post.publishedAt ? new Date(post.publishedAt) : new Date()}
-            slug={post.slug?.toString() ?? ""}
-          />
-        </div>
-      ))}
-    </div>
+    <ScrollArea className="w-lg">
+      <div className="mt-6 w-full">
+        {posts.map((post: Post, index: number) => (
+          <div
+            key={post.slug?.toString()}
+            className={`py-4 ${index !== posts.length - 1 ? "border-b border-gray-200" : ""}`}
+          >
+            <WritingListItem
+              title={post.title ?? ""}
+              date={post.publishedAt ? new Date(post.publishedAt) : new Date()}
+              slug={post.slug?.toString() ?? ""}
+            />
+          </div>
+        ))}
+      </div>
+    </ScrollArea>
   );
 }
--- src/app/(blog)/writing/page.tsx
+++ src/app/(blog)/writing/page.tsx
@@ -8,11 +8,18 @@ export const revalidate = 60; // revalidate this page every 60 seconds
 
 export default async function Blog() {
   return (
-    <main className="">
-      <div className="px-4 pt-8">
-        <h1 className="text-xl font-bold">Writing</h1>
+    <main className="h-screen">
+      <div className="flex min-h-full flex-row">
+        <div>
+          <div className="px-4 pt-8">
+            <h1 className="font-bold text-xl">Writing</h1>
+          </div>
+          <Posts />
+        </div>
+        <div className="flex min-h-full w-full items-center justify-center bg-red-500 text-4xl">
+          Writing
+        </div>
       </div>
-      <Posts />
     </main>
   );
 }
