d3a75e77f2dfeabdb1965b6e2439b73835d8036e
Justus Hebenstreit <justus@hebenstreit.io>
2025-05-13 21:05:01 +0100

    Start using prefetch

--- src/app/(blog)/writing/_components/writing-list-item.tsx
+++ src/app/(blog)/writing/_components/writing-list-item.tsx
@@ -21,31 +21,35 @@ export function WritingListItem({
   const isActive = pathname === `/writing/${slug}`;
 
   return (
-    <Link href={`/writing/${slug}`} className="group block mb-1">
+    <Link
+      href={`/writing/${slug}`}
+      className="group mb-1 block"
+      prefetch={true}
+    >
       <div
-        className={`py-4 px-6 rounded-xl space-y-1 transition-colors ${
+        className={`space-y-1 rounded-xl px-6 py-4 transition-colors ${
           isActive ? "bg-black" : "hover:bg-slate-200"
         }`}
       >
         <h2
           className={`transition-colors ${
             isActive
-              ? "text-white font-semibold"
-              : "text-slate-800 group-hover:text-primary font-normal"
+              ? "font-semibold text-white"
+              : "font-normal text-slate-800 group-hover:text-primary"
           }`}
         >
           {title}
         </h2>
         <p
           className={`text-sm ${
-            isActive ? "text-slate-300" : "text-slate-400 font-light"
+            isActive ? "text-slate-300" : "font-light text-slate-400"
           }`}
         >
           {format(date, "MMMM d, yyyy")}
         </p>
         {excerpt && (
           <p
-            className={`text-xs line-clamp-2 ${isActive ? "text-slate-400" : "text-slate-500"}`}
+            className={`line-clamp-2 text-xs ${isActive ? "text-slate-400" : "text-slate-500"}`}
           >
             {excerpt}
           </p>
